ac48c30fffc98c57bbfd2685eab2ad22fc7c9500
vishal19881 <115573369+vishal19881@users.noreply.github.com>
2025-02-11 19:43:33 +0530

    Update AppState.jsx

--- src/context/AppState.jsx
+++ src/context/AppState.jsx
@@ -7,7 +7,8 @@ import "react-toastify/dist/ReactToastify.css";
 const AppState = (props) => {
   // const url = "http://localhost:1000/api";
 
-  const url = "https://mern-e-commerce-backend-xa9r.onrender.com/api";
+  const url = "https://mern-e-commerce-backend-1-syav.onrender.com/api";
+  
 
 
   const [products, setProducts] = useState([]);
